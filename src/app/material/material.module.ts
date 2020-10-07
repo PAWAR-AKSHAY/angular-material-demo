a8e1588d0b7154bab432fcbd329e3e8f85d906e2
Akshay Pawar <46777220+PAWAR-AKSHAY@users.noreply.github.com>
2020-10-07 19:42:47 +0530

    Added new material components

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,20 +3,35 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
-const MaterialComponents = [
+
+
+
+const material = [
   MatButtonModule,
   MatButtonToggleModule,
   MatIconModule,
-  MatBadgeModule
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatMenuModule,
+  MatListModule,
+  MatDividerModule
 ]
 
 @NgModule({
   imports: [
-    MaterialComponents
+    material
   ],
   exports: [
-    MaterialComponents
+    material
   ]
 })
 export class MaterialModule { }
